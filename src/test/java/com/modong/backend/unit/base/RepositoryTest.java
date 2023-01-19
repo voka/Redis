3febcf05fb416b774278dd7d60d80f7a2257d7c6
voka <chdlswhd7@naver.com>
2023-01-19 18:24:20 +0900

    Test: base RepositoryTest 파일 추가

---
+++ src/test/java/com/modong/backend/unit/base/RepositoryTest.java
@@ -0,0 +1,10 @@
+package com.modong.backend.unit.base;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class RepositoryTest {
+
+}
