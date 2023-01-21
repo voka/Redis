289b98e7f3286318fbc83829d552885a8b7abfe0
voka <chdlswhd7@naver.com>
2023-01-21 11:58:24 +0900

    Feat: MemberRepository에 함수 findByMemberId 추가

--- src/main/java/com/modong/backend/auth/member/MemberRepository.java
+++ src/main/java/com/modong/backend/auth/member/MemberRepository.java
@@ -1,7 +1,9 @@
 package com.modong.backend.auth.member;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
+  Optional<Member> findByMemberId(String memberId);
 }
