7c323efc6222492bb262ed198102b0736d9ac0e0
voka <chdlswhd7@naver.com>
2023-01-25 16:59:57 +0900

    Test: Jwt 인증 관련 인터셉터 추가 및 컨트롤러 테스트 설정 클래스 MockBean으로 추가 @MockBean으로 AuthConfig 클래스와 JwtTokenProvider 클래스 추가

---
+++ src/main/java/com/modong/backend/config/AuthConfig.java
@@ -0,0 +1,31 @@
+package com.modong.backend.config;
+
+import com.modong.backend.auth.JwtTokenProvider;
+import com.modong.backend.auth.support.AuthArgumentResolver;
+import com.modong.backend.global.Interceptor.AuthenticationInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthConfig implements WebMvcConfigurer {
+
+  private final JwtTokenProvider jwtTokenProvider;
+
+  @Override
+  public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> argumentResolvers) {
+    argumentResolvers.add(new AuthArgumentResolver(jwtTokenProvider));
+  }
+
+  @Override
+  public void addInterceptors(final InterceptorRegistry registry) {
+    registry.addInterceptor(new AuthenticationInterceptor(jwtTokenProvider))
+        .addPathPatterns("/api/v1/member/**")
+        .excludePathPatterns("/api/v1/register", "/api/v1/login");
+  }
+
+}
