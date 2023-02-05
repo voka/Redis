6ab74a85c128e4e2ac019ef14c580a599aeff150
voka <chdlswhd7@naver.com>
2023-02-05 17:03:38 +0900

    Update: application Request 이름 변경

--- src/main/java/com/modong/backend/domain/application/ApplicationController.java
+++ src/main/java/com/modong/backend/domain/application/ApplicationController.java
@@ -2,11 +2,12 @@ package com.modong.backend.domain.application;
 
 import com.modong.backend.auth.Dto.TokenResponse;
 import com.modong.backend.domain.application.Dto.ApplicationDetailResponse;
-import com.modong.backend.domain.application.Dto.ApplicationRequest;
+import com.modong.backend.domain.application.Dto.ApplicationCreateRequest;
 import com.modong.backend.domain.application.Dto.ApplicationSimpleResponse;
 import com.modong.backend.base.Dto.BaseResponse;
 import com.modong.backend.base.Dto.SavedId;
 import com.modong.backend.Enum.CustomCode;
+import com.modong.backend.domain.application.Dto.ApplicationUpdateRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
@@ -40,7 +41,6 @@ public class ApplicationController {
   @Operation(summary = "동아리의 전체 지원서 조회", description = "동아리의 ID를 이용해 작성한 모든 지원서를 조회한다.", responses = {
       @ApiResponse(responseCode = "200", description = "게시글 조회 성공", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ApplicationSimpleResponse.class))))
   })
-  @Parameter(name = "id", description = "동아리 ID",  example = "1")
   public ResponseEntity getApplicationsByClubId(@Valid @PathVariable(name="club_id") Long clubId){
     List<ApplicationSimpleResponse> applications = applicationService.findAllByClubId(clubId);
     return ResponseEntity.ok(new BaseResponse(applications, HttpStatus.OK.value(), CustomCode.SUCCESS_GET_LIST));
@@ -50,7 +50,6 @@ public class ApplicationController {
   @Operation(summary = "지원서 조회", description = "지원서의 ID를 이용해 작성한 지원서를 조회한다.", responses = {
       @ApiResponse(responseCode = "200", description = "지원서 조회 성공", content = @Content(schema = @Schema(implementation = ApplicationDetailResponse.class)))
   })
-  @Parameter(name = "id", description = "지원서 ID",  example = "1")
   public ResponseEntity getApplicationById(@Valid @PathVariable(name = "application_id") Long applicationId){
     ApplicationDetailResponse application = applicationService.findDetailById(applicationId);
     return ResponseEntity.ok(new BaseResponse(application,HttpStatus.OK.value(), CustomCode.SUCCESS_GET));
@@ -60,7 +59,6 @@ public class ApplicationController {
   @Operation(summary = "지원서 조회", description = "지원서의 링크 아이디를 이용해 작성한 지원서를 조회한다.", responses = {
       @ApiResponse(responseCode = "200", description = "지원서 조회 성공", content = @Content(schema = @Schema(implementation = ApplicationDetailResponse.class)))
   })
-  @Parameter(name = "urlId", description = "지원서 url ID ",  example = "uH9wk72MTr")
   public ResponseEntity getApplicationByUrlId(@Valid @PathVariable(name = "url_id") String urlId){
     ApplicationDetailResponse application = applicationService.findDetailByUrlId(urlId);
     return ResponseEntity.ok(new BaseResponse(application,HttpStatus.OK.value(), CustomCode.SUCCESS_GET));
@@ -69,18 +67,16 @@ public class ApplicationController {
   //지원서 생성
   @PostMapping("/application")
   @Operation(summary = "지원서를 생성한다.", description = "지원서를 생성한다.")
-  public ResponseEntity createApplication(@Valid @RequestBody ApplicationRequest applicationRequest){
-    SavedId savedId = new SavedId(applicationService.createApplication(applicationRequest));
+  public ResponseEntity createApplication(@Valid @RequestBody ApplicationCreateRequest ApplicationCreateRequest){
+    SavedId savedId = new SavedId(applicationService.createApplication(ApplicationCreateRequest));
     return ResponseEntity.ok(new BaseResponse(savedId, HttpStatus.CREATED.value(), CustomCode.SUCCESS_CREATE));
   }
 
   //지원서 수정(필수 질문 부분)
   @PutMapping("/application/{application_id}")
   @Operation(summary = "지원서 수정", description = "지원서의 ID를 이용해 작성한 지원서를 수정한다.")
-  @Parameter(name = "id", description = "지원서 ID",  example = "1")
-  public ResponseEntity updateEssentialQuestion(@Valid @PathVariable(name = "application_id") Long applicationId, @RequestBody ApplicationRequest applicationRequest){
-    SavedId savedId = new SavedId(applicationService.updateEssentialQuestion(applicationId,
-        applicationRequest));
+  public ResponseEntity updateEssentialQuestion(@Valid @PathVariable(name = "application_id") Long applicationId, @RequestBody ApplicationUpdateRequest applicationUpdateRequest){
+    SavedId savedId = new SavedId(applicationService.updateApplication(applicationId, applicationUpdateRequest));
     return ResponseEntity.ok(new BaseResponse(savedId, HttpStatus.OK.value(), CustomCode.SUCCESS_UPDATE));
 
   }
@@ -88,7 +84,6 @@ public class ApplicationController {
   //지원서 삭제
   @DeleteMapping("/application/{application_id}")
   @Operation(summary = "지원서 삭제", description = "지원서의 ID를 이용해 작성한 지원서를 삭제한다.")
-  @Parameter(name = "id", description = "지원서 ID",  example = "1")
   public ResponseEntity deleteApplicationById(@Valid @PathVariable(name = "application_id") Long applicationId) {
     applicationService.deleteApplication(applicationId);
     return ResponseEntity.ok(new BaseResponse(HttpStatus.OK.value(), CustomCode.SUCCESS_DELETE));
