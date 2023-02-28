796c77973459fc6fb3f77035961e7d87d4bdb51d
voka <chdlswhd7@naver.com>
2023-02-28 20:51:43 +0900

    Update: Url 리소스를 찾지 못할경우 NoHandlerFoundException 을 통해 404 error 반환하도록 controlleradvice 변경

--- src/main/java/com/modong/backend/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/modong/backend/global/handler/GlobalExceptionHandler.java
@@ -2,12 +2,12 @@ package com.modong.backend.global.handler;
 
 import com.modong.backend.base.Dto.ErrorResponse;
 import com.modong.backend.global.exception.BadRequestException;
-import com.modong.backend.global.exception.NotFoundException;
 import com.modong.backend.global.exception.auth.NoPermissionException;
 import com.modong.backend.global.exception.auth.UnAuthorizedException;
 import java.security.InvalidParameterException;
 import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 @Slf4j
@@ -46,7 +47,8 @@ public class GlobalExceptionHandler {
   }
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   @ExceptionHandler({
-      BadRequestException.class
+      BadRequestException.class,
+      MethodArgumentTypeMismatchException.class //enum 타입이 맞지 않을 경우 발생
   })
   public ResponseEntity<ErrorResponse> handleBadRequestException(final BadRequestException e) {
     log.error(ERROR_LOGGING_MESSAGE, e);
@@ -56,22 +58,20 @@ public class GlobalExceptionHandler {
         .code(e.getErrorCode()).build();
     return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
   }
-
+  // 404예외처리 핸들러
   @ResponseStatus(HttpStatus.NOT_FOUND)
-  @ExceptionHandler({
-      NotFoundException.class,
-      MethodArgumentTypeMismatchException.class
-  })
-  public ResponseEntity<ErrorResponse> handleNotFoundException(final NotFoundException e) {
+  @ExceptionHandler(NoHandlerFoundException.class)
+  public ResponseEntity<ErrorResponse> handle404(NoHandlerFoundException e){
     log.error(ERROR_LOGGING_MESSAGE, e);
 
     ErrorResponse response = ErrorResponse.builder()
-        .status(HttpStatus.NOT_FOUND.value())
-        .message(e.getClientMessage())
-        .code(e.getErrorCode()).build();
+            .status(HttpStatus.NOT_FOUND.value())
+            .code("리소스를 찾을 수 없습니다")
+            .message(e.getMessage()).build();
 
     return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
   }
+
   @ResponseStatus(HttpStatus.UNAUTHORIZED)
   @ExceptionHandler({
       UnAuthorizedException.class
@@ -106,7 +106,12 @@ public class GlobalExceptionHandler {
 
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   @ExceptionHandler
-  public void handleRuntimeException(final RuntimeException e, HttpServletRequest request) {
-    log.error("예상하지 못한 에러가 발생하였습니다. 요청 url : %s", request.getRequestURI() + ", 요청 메소드: %s" + request.getMethod(), e);
+  public ResponseEntity<ErrorResponse> handleRuntimeException(final RuntimeException e, HttpServletRequest request) {
+    log.error("예상하지 못한 에러가 발생하였습니다. 요청 url : ", request.getRequestURI(),e);
+    ErrorResponse response = ErrorResponse.builder()
+            .status(HttpStatus.FORBIDDEN.value())
+            .message("예상하지 못한 에러가 발생하였습니다.")
+            .code("SERVER ERROR").build();
+    return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
